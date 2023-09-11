69acf3ce221440580c5653ec3b54246278a93a74
Mod275 <mo.gadalla27@gmail.com>
2023-09-10 20:23:15 -0700

    1-compiler

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(void) {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+
+/**
+ *main first function to execute 
+ *
+ *printf method
+ *
+ *always main return 0 
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
